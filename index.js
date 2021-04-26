32eb28e25a08d937f566e7781cbf7d5babbf98df
Chadwick Smith <chadwickmini@Chadwicks-Mini.hsd1.ct.comcast.net>
2021-04-26 19:51:50 -0400

    added get request for specific team id

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require('express')
+const teams = require('./teams.js')
 const app = express()
 
-app.get('/',(request,response) => {
- 
+app.get('/teams',(req,res) => {
+    return res.send(teams)
+
+})
+app.get('/teams/:id', (req,res) => {
+    const team = teams.find((team) => {
+            return team.id === 
+            parseInt(req.params.id)})
+    return res.send(team)
 })
 
 
-
+app.all('*', (req,res) => {
+    return res.sendStatus(404)
+})
 
 app.listen(3370, () => {
     console.log('listening on 3370...')
